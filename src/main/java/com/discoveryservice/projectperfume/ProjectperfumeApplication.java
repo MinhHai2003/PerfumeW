6c785cdd5cea87cc995ade2d3d90ae8304657365
Minh Hai <hainguyenminh.20039@gmail.com>
2025-02-26 23:33:38 +0700

    thiet lap microsoft vs springboot

---
+++ src/main/java/com/discoveryservice/projectperfume/ProjectperfumeApplication.java
@@ -0,0 +1,13 @@
+package com.discoveryservice.projectperfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectperfumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectperfumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discoveryservice/projectperfume/ProjectperfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discoveryservice.projectperfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectperfumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
